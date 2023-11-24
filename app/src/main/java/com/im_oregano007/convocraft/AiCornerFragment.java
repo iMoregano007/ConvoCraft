285de61e3fff64a6414c473c9f228fe60e713f50
iMoregano007 <imgauravs84@gmail.com>
2023-11-24 17:43:36 +0530

    Day 3 : Main activity design

---
+++ app/src/main/java/com/im_oregano007/convocraft/AiCornerFragment.java
@@ -0,0 +1,26 @@
+package com.im_oregano007.convocraft;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class AiCornerFragment extends Fragment {
+
+
+
+    public AiCornerFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_ai_corner, container, false);
+    }
+}
--- app/src/main/java/com/im_oregano007/convocraft/MainActivity.java
+++ app/src/main/java/com/im_oregano007/convocraft/MainActivity.java
@@ -1,14 +1,55 @@
 package com.im_oregano007.convocraft;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ImageButton;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    ImageButton searchBtn;
+    ChatFragment chatFragment;
+    AiCornerFragment aiCornerFragment;
+    ProfileFragment profileFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        chatFragment = new ChatFragment();
+        aiCornerFragment = new AiCornerFragment();
+        profileFragment = new ProfileFragment();
+        bottomNavigationView = findViewById(R.id.main_navigation_view);
+        searchBtn = findViewById(R.id.search_button);
+
+        searchBtn.setOnClickListener((v) ->{
+            startActivity(new Intent(MainActivity.this,SearchUserActivity.class));
+        });
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                if(item.getItemId() == R.id.menu_chat){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.main_frame,chatFragment).commit();
+                }
+                if(item.getItemId() == R.id.menu_ai){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.main_frame,aiCornerFragment).commit();
+                }
+                if(item.getItemId() == R.id.menu_profile){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.main_frame,profileFragment).commit();
+                }
+                return true;
+            }
+        });
+
+        bottomNavigationView.setSelectedItemId(R.id.menu_chat);
     }
 }
